3db9aba35513485334cb1a4ac37d12841907a52a
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-15 13:06:39 -0700

    Update setup_mysql_dev.sql and setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,6 +9,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
