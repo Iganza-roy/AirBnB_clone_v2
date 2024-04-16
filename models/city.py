f99cacd2c2c769b581392390170eaa0ab17ffce5
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-16 03:10:40 -0700

    mapped city to states & places

--- models/city.py
+++ models/city.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column, Foreignkey, String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ Reps a City for MySQL database
+    Uses SQLAlchemy to link to MySQL cities table
+
+    Attrs:
+        __tablename__(str): cities table name
+        name (SQLAlchemy str): name of City
+        state_id (SQLAlchemy str): FK that links cities to states
+        
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
