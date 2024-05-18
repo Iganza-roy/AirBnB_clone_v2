bc7435e859738ea326fc17d46398750585622bf7
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-18 14:24:59 -0700

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,14 @@
 from flask import Flask
 """Flask web app"""
 
-app = Flasik(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
     """starts a Flask web application"""
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
