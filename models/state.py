129f2d02138b0d0463a5b3c2447b6170f68e4b31
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-19 14:53:51 -0700

    Update models/state.py

--- models/state.py
+++ models/state.py
@@ -32,7 +32,7 @@ class State(BaseModel, Base):
             """
             from models import storage
             cities_list = []
-            for city in storage.all("City").values():
+            for city in models.storage.all("City").values():
                 if city.state_id == self.id:
                     cities_list.append(city)
             return cities_list
