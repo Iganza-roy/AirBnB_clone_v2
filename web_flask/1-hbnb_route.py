5b6f6048411215598bb0412b73eff76d4c0d741a
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-19 13:00:27 -0700

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """landing page"""
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """HBNB route"""
     return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
