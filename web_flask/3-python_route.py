118454d0e0dcf6e55699c611dda65ecc95306c5e
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-19 13:23:38 -0700

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_route(text):
     return f"C {text}"
 
 
-@app.route('/python/', defaults={'text':'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_route(text):
     """display “Python ” followed by the value of the text variable"""
